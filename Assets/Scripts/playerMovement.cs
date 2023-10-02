27b157aaf9b7c9d464f211a4b88ca0b76496a5cf
WalkanFL <maciej.b.szczepanski@o2.pl>
2023-10-02 18:07:56 +0200

    Flashlight Functionality
    it works :DD

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,20 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
     public Rigidbody controller;
     public float movementSpeed = 5f;
+    public Transform flashlight;
+    public GameObject[] flashlightPositions;
+    private float horizontal;
+    private float vertical;
 
     Vector3 direction;
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
         direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        Vector3 inputedDirection = flashlight.position;
+        Quaternion inputedRotation = flashlight.transform.rotation;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            inputedDirection = Vector3.Slerp(flashlight.position, flashlightPositions[2].transform.position, movementSpeed);
+            inputedRotation = Quaternion.Slerp(flashlight.rotation, flashlightPositions[2].transform.rotation, movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            inputedDirection = Vector3.Slerp(flashlight.position, flashlightPositions[3].transform.position, movementSpeed);
+            inputedRotation = Quaternion.Slerp(flashlight.rotation, flashlightPositions[3].transform.rotation, movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            inputedDirection = Vector3.Slerp(flashlight.position, flashlightPositions[1].transform.position, movementSpeed);
+            inputedRotation = Quaternion.Slerp(flashlight.rotation, flashlightPositions[1].transform.rotation, movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            inputedDirection = Vector3.Slerp(flashlight.position, flashlightPositions[0].transform.position, movementSpeed);
+            inputedRotation = Quaternion.Slerp(flashlight.rotation, flashlightPositions[0].transform.rotation, movementSpeed);
+        }
+
+        flashlight.position = inputedDirection;
+        flashlight.transform.rotation = inputedRotation;
+
 
     }
 
