19f2a4dfae0e7e5914f15ad007ff20682bebc839
maksslom <103206298+maksslom@users.noreply.github.com>
2023-10-03 21:38:57 +0200

    Added Enemy
    Added an enemy prefab and an enemy movement script letting it chase the player and randomly move around the map

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkpoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    
+
+    void Awake()
+    {
+     player = GameObject.Find("PlayerObject").transform;
+     agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (!playerInSightRange) Patroling();
+        if (playerInSightRange) ChasePlayer();
+    }
+
+
+    void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkpoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkpoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkpoint, -transform.up, 2f,whatIsGround))
+            walkPointSet = true;
+    }
+
+    void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+}
